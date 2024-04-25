021a9550e5016c94efbbe19058712be2f4308487
Monica Sharma <msharma368!@yahoo.com>
2024-04-25 17:42:33 -0400

    Code for workshop 2

---
+++ src/Main.java
@@ -0,0 +1,207 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
+package com.ps;
+import java.awt.print.Book;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] books = {"Honda", "Toyota", "Lexus","Cat in the Hat","My ABC","Cats" };
+        // Need to list books probably declare a book variable. Then declare other strings can't figure out if i should use array or book example
+        String[] isbn =  { "978-93-5300-895-6 -GS1","878-93-5300-895-6 -bS1 " };
+        //need to set a boolean value for being checkout.Create book class,then getter and setter
+        boolean isCheckedOut = true;//going to be a property in the book class,once is book is done then
+        System.out.println(isCheckedOut);
+// need to figure out how to set initals to be entererd
+        String initials;
+        System.out.print("Enter initials");
+
+        // id, create book class
+
+
+
+
+
+
+        Book book1 = new Book (1,"Cat in the Hat");
+        Book book2 = new Book (2,"My ABC");
+        Book book3= new Book (3,"MY 123");
+        Book book4 = new Book (4"Cats");
+        Book book5 = new Book (5"Bats");
+        Book book6 = new Book (6,"Mouse");
+        Book book7 = new Book (7,"Spelling");
+        Book book8 = new Book (8,"Knitting");
+        Book book9 = new Book (9,"Counting");
+        Book book10 = new Book (10,"Cooking");
+        Book book11 = new Book (11,"Cleaning");
+        Book book12 = new Book (12,"Carriages");
+        Book book13 = new Book (13,"Capybaras");
+        Book book14 = new Book (14,"Cougars");
+        Book book15 = new Book (15,"Columbus");
+        Book book16 = new Book (16,"Canary Islands");
+        Book book17 = new Book (17,"Columbia");
+        Book book18 = new Book (18,"Catalonia");
+        Book book19 = new Book (19,"China");
+        Book book20= new Book (20,"Cryogenics");
+
+    }
+
+package com.ps;
+import java.util.scanner
+import java.util.arrays
+
+
+    public class Book {
+        public void static main(String[] args) {
+
+
+            class Book{
+
+                private int id;
+
+                private String isbn;
+
+                private String title;
+
+                private boolean is CheckedOut;
+
+                private String checkedOutTo;
+
+                public String getIsbn() [
+                  return isbn;
+
+                    ]
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+    }
+
+    //constructors, setter ans getters
+    public class Book {
+        public static void main(String[] args) {
+            //what needs to included title,id,ibsn,name of person checking in/out,chekc out status.List books similar to plant exeercise.Include other traits in line
+            Book book1 = new Book(id 1,title "Cat in the Hat", isbn "ISBN-90471", isCheckedOut "True", isCheckedOut "Mary Jo"                                            );
+            Book book2 = new Book(id 2,title "My ABC" isbn "ISBN-90371" isCheckedOut "True" , isCheckedOut "Trey Lo"                           );
+            Book book3=  new Book(id 3,title "MY 123" isbn "ISBN-90821" isCheckedOut "True"  , isCheckedOut "Tove Lo"                   );
+            Book book4 = new Book(id 4,title "Cats"); isbn "ISBN-90841" isCheckedOut "True" , isCheckedOut "Rey So"
+            Book book5 = new Book(id 5,title "Bats");  isbn "ISBN-90831" isCheckedOut "True" ,isCheckedOut "Ey bo"
+            Book book6 = new Book(id 6,title "Mouse"); isbn "ISBN-92871" isCheckedOut "True" , isCheckedOut " May Lo"
+            Book book7 = new Book(id 7,title "Spelling" isbn "ISBN-90571" isCheckedOut "True" , isCheckedOut " May Lo"                        );
+            Book book8 = new Book(id 8,title "Knitting" isbn "ISBN-98871" isCheckedOut "True"  , isCheckedOut " May Lo"                   );
+            Book book9 = new Book(id 9,title "Counting" isbn "ISBN-00871"  isCheckedOut "True"  , isCheckedOut " May Lo"                        );
+            Book book10 = new Book(id 10,title "Cooking" isbn "ISBN-30871"  isCheckedOut "True"  , isCheckedOut " May Lo"                       );
+            Book book11 = new Book(id 11,title "Cleaning" isbn "ISBN-50871"  isCheckedOut "True"  , isCheckedOut " May Lo"                     );
+            Book book12 = new Book(id 12,title "Carriages"  isbn "ISBN-40871" isCheckedOut "True" , isCheckedOut " May Lo"                        );
+            Book book13 = new Book(id 13,title  "Capybaras" isbn "ISBN-60871"   isCheckedOut "True" , isCheckedOut " May Lo"                     );
+            Book book14 = new Book(id 14,title "Cougars" isbn "ISBN-10871"   ); isCheckedOut "True" , isCheckedOut " May Lo"
+            Book book15 = new Book(id 15,title "Columbus" isbn "ISBN-80871");   isCheckedOut "True" , isCheckedOut " May Lo"
+            Book book16 = new Book(id 16,title "Canary Islands" isbn "ISBN-90871" isCheckedOut "True" , isCheckedOut " May Lo"            );
+            Book book17 = new Book(id 17,title "Columbia"   isbn "ISBN-90841" isCheckedOut "True"  , isCheckedOut " May Lo"                   )  ;
+            Book book18 = new Book(id 18,title "Catalonia" isbn "ISBN-90851" isCheckedOut "True" , isCheckedOut " May Lo"                       );
+            Book book19 = new Book(id 19,title "China"isbn "ISBN-90861");   isCheckedOut "True" , isCheckedOut " May Lo"
+            Book book20= new Book( id 20,title "Cryogenics",isbn "ISBN-90871" isCheckedOut "True"  , isCheckedOut " May Lo"  );
+
+
+            package com.ps;
+import java.util.scanner
+import java.util.arrays
+
+
+            public class Book {
+                public void static main(String[] args) {
+
+
+                    class Book{
+
+                        private int id;
+
+                        private String isbn;
+
+                        private String title;
+
+                        private boolean is CheckedOut;
+
+                        private String checkedOutTo;
+
+                        public String getIsbn() [
+                  return isbn;
+
+                    ]
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+
+                }
+
+
+            }
+
+//constructors, setter ans getters
+            public class Book {
+                public static void main(String[] args) {
+                    //what needs to included title,id,ibsn,name of person checking in/out,chekc out status.List books similar to plant exeercise.Include other traits in line
+                    Book book1 = new Book(id 1,title "Cat in the Hat", isbn "ISBN-90471", isCheckedOut "True", isCheckedOut "Mary Jo"                                            );
+                    Book book2 = new Book(id 2,title "My ABC" isbn "ISBN-90371" isCheckedOut "True" , isCheckedOut "Trey Lo"                           );
+                    Book book3=  new Book(id 3,title "MY 123" isbn "ISBN-90821" isCheckedOut "True"  , isCheckedOut "Tove Lo"                   );
+                    Book book4 = new Book(id 4,title "Cats"); isbn "ISBN-90841" isCheckedOut "True" , isCheckedOut "Rey So"
+                    Book book5 = new Book(id 5,title "Bats");  isbn "ISBN-90831" isCheckedOut "True" ,isCheckedOut "Ey bo"
+                    Book book6 = new Book(id 6,title "Mouse"); isbn "ISBN-92871" isCheckedOut "True" , isCheckedOut " May Lo"
+                    Book book7 = new Book(id 7,title "Spelling" isbn "ISBN-90571" isCheckedOut "True" , isCheckedOut " May Lo"                        );
+                    Book book8 = new Book(id 8,title "Knitting" isbn "ISBN-98871" isCheckedOut "True"  , isCheckedOut " May Lo"                   );
+                    Book book9 = new Book(id 9,title "Counting" isbn "ISBN-00871"  isCheckedOut "True"  , isCheckedOut " May Lo"                        );
+                    Book book10 = new Book(id 10,title "Cooking" isbn "ISBN-30871"  isCheckedOut "True"  , isCheckedOut " May Lo"                       );
+                    Book book11 = new Book(id 11,title "Cleaning" isbn "ISBN-50871"  isCheckedOut "True"  , isCheckedOut " May Lo"                     );
+                    Book book12 = new Book(id 12,title "Carriages"  isbn "ISBN-40871" isCheckedOut "True" , isCheckedOut " May Lo"                        );
+                    Book book13 = new Book(id 13,title  "Capybaras" isbn "ISBN-60871"   isCheckedOut "True" , isCheckedOut " May Lo"                     );
+                    Book book14 = new Book(id 14,title "Cougars" isbn "ISBN-10871"   ); isCheckedOut "True" , isCheckedOut " May Lo"
+                    Book book15 = new Book(id 15,title "Columbus" isbn "ISBN-80871");   isCheckedOut "True" , isCheckedOut " May Lo"
+                    Book book16 = new Book(id 16,title "Canary Islands" isbn "ISBN-90871" isCheckedOut "True" , isCheckedOut " May Lo"            );
+                    Book book17 = new Book(id 17,title "Columbia"   isbn "ISBN-90841" isCheckedOut "True"  , isCheckedOut " May Lo"                   )  ;
+                    Book book18 = new Book(id 18,title "Catalonia" isbn "ISBN-90851" isCheckedOut "True" , isCheckedOut " May Lo"                       );
+                    Book book19 = new Book(id 19,title "China"isbn "ISBN-90861");   isCheckedOut "True" , isCheckedOut " May Lo"
+                    Book book20= new Book( id 20,title "Cryogenics",isbn "ISBN-90871" isCheckedOut "True"  , isCheckedOut " May Lo"  );
+
+
+
+
+
+
